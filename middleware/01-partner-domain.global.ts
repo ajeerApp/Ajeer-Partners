17bc9a22d75a56605f1bffaea0c11921d375d42e
omaralalwi <o.alalwi@iajeer.com>
2023-12-11 06:52:39 +0300

    clean up

--- middleware/01-partner-domain.global.ts
+++ middleware/01-partner-domain.global.ts
@@ -6,23 +6,16 @@ import { useRouter } from 'vue-router';
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const routeSubDomain = to.params.subdomain;
   const subDomain = getSubDomain();
-  console.log('subDomain middleware is: ', subDomain );
+  console.log('routeSubDomain is',routeSubDomain , 'subDomain is',subDomain );
   if (!subDomain || !isValidSubdomain(subDomain, routeSubDomain)) {
-    console.log('partner domain is not Valid, subDomain is: ', subDomain);
+    console.log('partner domain is not Valid',);
     throw createError({ statusCode: 404, statusMessage: 'Page Not Found' })
   }
 });
 
 function isValidSubdomain(subDomain, routeSubDomain) {
   const partner = partnerInfo();
-  // TODO, make api request to backend to get partner info
-  // TODO, handle this by api with dashboard
-  // if(isValidPartner()) {
-  //   return true;
-  // } else {
-  //   partner.getInfo({});
-  // }
-  // TODO, send request to endpoint to get all partners domains , for checking
+  // TODO, send request to endpoint to get all partners domains, for checking
   const validSubdomains = ['saudiceramics', 'test1'];
   return validSubdomains.includes(subDomain) && subDomain === routeSubDomain;
 }
