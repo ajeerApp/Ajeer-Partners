c1d57549b9e22bb6bb7f0ac83f2d441dd0a8e6e1
ajeer-alsuhaibi <a.alsuhaibi@iajeer.com>
2023-12-21 02:28:04 +0300

    chore: uncomment unsed function in orders.js

--- stores/orders.js
+++ stores/orders.js
@@ -19,31 +19,6 @@ export const useOrdersStore=defineStore('Orders',{
     ]
         }),
     actions:{
-        //set value of order
-//         updateOrders(){
-//             const authStore = useAuth();
-//             const authOrders = authStore.orders;
-
-//             if(authOrders){
-//             // Clear current orders
-//              this.orders = [];
-
-//             }
-//            console.log("authStore.orders()",authOrders)
-//            this.orders=authOrders;
-//             // for(let i=0;i<orders.lenght;i++){
-//             //     this.orders[i].id=orders[i].id
-//             //     this.orders[i].delivery_status=orders[i].delivery_status
-//             //     this.orders[i].purchase_date=orders[i].id
-//             //     for(let j=0;j<orders.products.lenght;j++){
-//             //         this.orders[i].products[j].name=orders.products[i].name
-//             //         this.orders[i].products[j].quantity=orders.products[i].quantity
-//             //         this.orders[i].products[j].image=orders.products[i].image
-//             //       }
-//             //   }
-          
-// console.log("orders",this.orders)
-//         }
 setOrders(newOrders) {
     this.orders= newOrders;
   }
