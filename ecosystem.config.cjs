ea5828d9b127d7ee60711a89354d868602180809
Omar Alalwi <o.alalwi@iajeer.com>
2023-12-27 05:12:44 +0300

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
             port:'3003',
             script: './.output/server/index.mjs',
             exec_mode: 'cluster',
-            instances: 'max',
+            instances: '1',
             mode: 'production',
         },
         {
@@ -13,7 +13,7 @@ module.exports = {
             port:'3003',
             script: './.output/server/index.mjs',
             exec_mode: 'cluster',
-            instances: 'max',
+            instances: '1',
             mode: 'stg',
         },
     ]
